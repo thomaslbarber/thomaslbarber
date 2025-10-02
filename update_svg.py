c37c8e206cd9a9a74f4b622db7e33579f9cb0e6f
Thomas Barber <67984922+thomaslbarber@users.noreply.github.com>
2025-10-02 22:52:02 +0100

    Update update_svg.py

--- update_svg.py
+++ update_svg.py
@@ -1,15 +1,12 @@
 import requests
 import datetime
 
-# GitHub repo info
 OWNER = "thomas"
 REPO = "thomaslbarber"
 TOKEN = "${{ secrets.GITHUB_TOKEN }}"
 
-# Time window (past 7 days)
 since = (datetime.datetime.utcnow() - datetime.timedelta(days=7)).isoformat() + "Z"
 
-# Fetch commits
 url = f"https://api.github.com/repos/{OWNER}/{REPO}/commits"
 params = {"since": since}
 headers = {"Authorization": f"token {TOKEN}"}
@@ -19,13 +16,10 @@ commits = r.json()
 
 commit_count = len(commits)
 
-# Read template
 with open("tamagotchi.svg", "r") as f:
     svg = f.read()
 
-# Replace placeholder
 svg = svg.replace("{{COMMITS}}", str(commit_count))
 
-# Write updated SVG
 with open("dist/tamagotchi.svg", "w") as f:
     f.write(svg)
